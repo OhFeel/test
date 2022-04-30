3baff32f52d93b1fde4e6289fcb34e30a5e37f74
Mats <Matsdenhoed@gmail.com>
2022-04-30 02:25:35 +0200

    idk

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,13 +1,14 @@
 const projects = [
-    {
-        "name": "Uberbot",
-        "description": "A cool verified discord bot",
-        "url": "uberbot.cf"
-    },
+    
     {
         "name": "Sussy devs project",
         "description": "A project for TCOC. ",
         "url": "https://github.com/OhFeel/sussy-devs-project"
+    },
+    {
+        "name": "Uberbot",
+        "description": "A cool verified discord bot",
+        "url": "https://uberbot.cf"
     },
     {
         "name": "Email spammer",
